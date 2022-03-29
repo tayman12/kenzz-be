b827e284cafc80bcc8a9d7948620af4838aa6bfd
Tocka Ayman <tocka.ayman@outlook.com>
2022-03-29 21:15:56 +0200

    Initial commit

---
+++ src/main/java/com/kenzz/be/KenzzBeApplication.java
@@ -0,0 +1,13 @@
+package com.kenzz.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KenzzBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KenzzBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kenzz/be/KenzzBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenzz.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KenzzBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
